eb80c8d8f6d7b334696a0ddec5760dafb2b92397
HusseinKanaan <107847813+HusseinKanaan@users.noreply.github.com>
2022-07-15 01:15:24 +0200

    commit

---
+++ assets /js/main.js
@@ -0,0 +1,11 @@
+document.write("<h1> Hello world </h1><p>Have a nice day!</p>")
+
+let myText='<p>"Hello again </p>"'
+console.log(myText)
+
+document.write(myText);
+
+let vorname= 'Hussein'
+let name='Kanaan'
+
+ document.write(vorname+ " " +name)
